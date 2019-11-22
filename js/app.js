1f4ac55694a953cc98a9d28a514755fbbe6622ab
ByStander-Ox <SergioXD753@hotmail.com>
2019-11-22 15:06:23 -0500

    ewe

---
+++ js/app.js
@@ -0,0 +1,57 @@
+var options = {
+  strings: [
+    "Hola me llamo Sergio Vásquez",
+    "Me gusta aprender",
+    "Me gusta saber",
+    "Me gusta pensar",
+    "Me gusta conocer",
+    "Me gusta entender",
+    "Me gusta crear"
+  ],
+  typeSpeed: 100,
+  loop: true
+};
+
+var type = new Typed(".type", options);
+
+var typed = new Typed("#typed", {
+  stringsElement: "#typed-strings"
+});
+
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+var wrapper = document.querySelector(".wrapper svg");
+var btnDraw = document.querySelector(".btn-draw");
+var btnErase = document.querySelector(".btn-erase");
+
+// We are only adding and removing the 'active' class,
+// the entire animation is defined in the CSS code
+function draw() {
+  wrapper.classList.add("active");
+}
+
+function erase() {
+  wrapper.classList.remove("active");
+}
+
+// Add handlers to our buttons
+btnDraw.addEventListener("click", draw, false);
+btnErase.addEventListener("click", erase, false);
+
+// Play draw animation once
+setTimeout(draw, 300);
