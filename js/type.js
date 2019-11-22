64c3a2718342860ee6844078c225799a361bdbe9
ByStander-Ox <SergioXD753@hotmail.com>
2019-11-22 18:59:33 -0500

    plz work

---
+++ js/type.js
@@ -0,0 +1,11 @@
+var typed2 = new Typed("#typed2", {
+  strings: [
+    "Some <i>strings</i> with",
+    "Some <strong>HTML</strong>",
+    "Chars &times; &copy;"
+  ],
+  typeSpeed: 0.5,
+  backSpeed: 0.5,
+  fadeOut: true,
+  loop: true
+});
