fe40f27d45b7664151d690c08d80e4aec670565a
ByStander-Ox <SergioXD753@hotmail.com>
2019-11-22 22:36:07 -0500

    falta el responsive del titulo

--- js/type.js
+++ js/type.js
@@ -1,10 +1,22 @@
+var typed = new Typed("#typed", {
+  strings: [
+    "No hay mucho que resaltar en cuanto a mis habilidades por ahora mas que lo que prodrias inferir por el diseño de la página y poco mas. Siempre intento aprender cosas nuevas pero usualmente me quedo contento con saber lo básico o simplemente conocerlas"
+  ],
+  typeSpeed: 100,
+
+  fadeOut: true,
+  loop: false
+});
 var typed2 = new Typed("#typed2", {
   strings: [
-    "Some <i>strings</i> with",
-    "Some <strong>HTML</strong>",
-    "Chars &times; &copy;"
+    "Me llamo Sergio Vásquez",
+    "Soy <strong>Estudiante</strong>",
+    "Me gusta Aprender;",
+    "Me gusta Conocer",
+    "Me gusta Programar",
+    "Me gusta el Pan"
   ],
-  typeSpeed: 0.5,
+  typeSpeed: 200,
   backSpeed: 0.5,
   fadeOut: true,
   loop: true
